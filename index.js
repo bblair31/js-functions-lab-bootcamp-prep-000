86ece2cdf9ab34596724529984d2c8238e61c3e5
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-09 18:59:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 function happyHolidaysTo($name) {
-  console.log ('Happy holidays,')
+  console.log ("Happy holidays,")
   return ($name)
 }
